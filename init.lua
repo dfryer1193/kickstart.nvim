15514518a0e96955e37f0c2846235f4995314990
David Fryer <dfryer1193@gmail.com>
2025-03-23 20:36:38 -0400

    Run goimports before saving go files

--- init.lua
+++ init.lua
@@ -762,6 +762,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        go = { 'goimports' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
